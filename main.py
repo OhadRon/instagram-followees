0f3cb9703edf8b7ee1db4eb50cb0badc751c6d2a
Ohad Ron <ohdron@gmail.com>
2015-09-28 13:52:47 +0300

    Moved all the time consuming tasks to a worker

--- main.py
+++ main.py
@@ -1,148 +1,25 @@
 import requests
 import os
 import sys
-from datetime import datetime, timedelta, date
+import uuid
 from bottle import route, run, request, template, redirect, post
 from rq import Queue
 from worker import conn
+import utils
+from redis import Redis
 
 q = Queue(connection=conn)
 
 session = requests.Session()
 
-class Followee:
-	def __init__(self, id, username, profile_pic):
-		self.recent_media = []
-		self.user_id = id
-		self.username = username
-		self.profile_pic = profile_pic
-
-	def getRecentMedia(self,payload):
-		r = session.get('https://api.instagram.com/v1/users/'+self.user_id+'/media/recent/', params=payload)
-		if r.status_code == 200: # If not, this user is probably visible to followers only
-			for item in r.json()['data']:
-				self.recent_media.append({'time':float(item['created_time']), 'user': item['user']['username'], 'likes': int(item['likes']['count'])})
-
-	def photosPerDay(self):
-		days = {}
-		most_days_back = datetime.now().date()-datetime.fromtimestamp(self.recent_media[-1]['time']).date()
-		most_days_back = most_days_back.days
-
-		for day in xrange(0,most_days_back):
-			days[day] = 0
-
-		for photo in self.recent_media:
-			photo_day = datetime.now().date()-datetime.fromtimestamp(photo['time']).date()
-			photo_day = photo_day.days
-			if photo_day < most_days_back:
-				days[photo_day] += 1
-
-		num_of_photos = 0
-		for day in xrange(0,most_days_back):
-			num_of_photos += days[day]
-
-		return format(num_of_photos / float(most_days_back), '.2f')
-
-	def likesPerPhoto(self):
-		likes = 0
-		for photo in self.recent_media:
-			likes += photo['likes']
-
-		return format(likes / float(len(self.recent_media)), '.2f')
-
-
-	def __repr__(self):
-		return "<Followee %s, %s, %s>"%(str(self.username), str(self.user_id), str(len(self.recent_media)))
-
-	def printData(self):
-		if len(self.recent_media) != 0:
-			photos_per_day = self.photosPerDay()
-			likes_per_photo = self.likesPerPhoto()
-			photo_time = self.recent_media[0]['time']
-			print self.username,
-			print '\t',
-			print photo_time,
-			print '\t',
-			print photos_per_day,
-			print '\t',
-			print likes_per_photo
-			return {'username': self.username, 'photos_per_day': photos_per_day, 'likes_per_photo': likes_per_photo, 'last_photo_time': photo_time, 'id' : self.user_id, 'profile_pic': self.profile_pic}
-		else:
-			print self.username
-			return {'username': self.username}
-
-
-
-def main(access_token):
-
-	CLIENT_ID=os.environ['CLIENT_ID']
-
-	followees = []
-
-	response = {}
-
-	payload = {'client_id':CLIENT_ID, 'access_token': access_token}
-
-	def getUser():
-		r = session.get('https://api.instagram.com/v1/users/self/', params=payload)
-		return r.json()
-
-	user_info = getUser()
-	
-	print "User name is", user_info['data']['username']
-
-	response['self_user'] = user_info['data']['username']
-
-	print "User has", user_info['data']['counts']['follows'], "followings"
-
-	response['followings'] = user_info['data']['counts']['follows']
-
-	def populateFollowings(user_id):
-		paginated = True
-		current_url = 'https://api.instagram.com/v1/users/'+str(user_id)+'/follows'
-		while paginated:
-			r = session.get(current_url, params=payload)
-			for item in r.json()['data']:
-				followees.append(Followee(item['id'], item['username'], item['profile_picture']))
-			if 'next_url' in r.json()['pagination']:
-				current_url = r.json()['pagination']['next_url']
-			else: 
-				paginated = False
-		print len(followees), "Followings found"
-		return True
-
-	print "Getting followings info..."
-	populateFollowings(user_info['data']['id'])
-
-	response['users'] = []
-	print "Getting followings media..."
-
-	if os.environ['DEBUG_MODE'] == 'TRUE':
-		# Only get the first 5 followees to conserve time
-		print "[DEBUG] Getting only first 5 followees"
-		for user in followees[:5]:
-			user.getRecentMedia(payload)
-			response['users'].append(user.printData())
-	else:
-		for user in followees:
-			user.getRecentMedia(payload)
-			response['users'].append(user.printData())
-
-	return response
-
-
 @route('/')
-def hello():	
-	return template('before.html', redirect_url = os.environ['BASE_URL']+'/results', client_id = os.environ['CLIENT_ID'])
-
-@route('/results')
-def results():
+def start():
 	if request.query.get('code') is not None:
 		payload = { 
 			'client_id': os.environ['CLIENT_ID'],
 			'client_secret': os.environ['CLIENT_SECRET'],
 			'grant_type': 'authorization_code',
-			'redirect_uri': os.environ['BASE_URL']+'/results',
+			'redirect_uri': os.environ['BASE_URL'],
 			'code': request.query.get('code'),
 			'scope': 'basic+relationships',
 		}
@@ -152,9 +29,99 @@ def results():
 		if 'access_token' in response:
 			return template('answer.html', access_token=response['access_token'])
 		else:
-			redirect('/')
+			return template('before.html', redirect_url = os.environ['BASE_URL'], client_id = os.environ['CLIENT_ID'])
 	else:
-		redirect('/')
+		return template('before.html', redirect_url = os.environ['BASE_URL'], client_id = os.environ['CLIENT_ID'])
+
+
+@route('/get_data')
+def get_data():
+	if request.query.get('access_token') is not None:
+		CLIENT_ID=os.environ['CLIENT_ID']
+
+		followees = []
+
+		response = {}
+
+		payload = {'client_id':CLIENT_ID, 'access_token': request.query.get('access_token')}
+
+		def getUser():
+			r = session.get('https://api.instagram.com/v1/users/self/', params=payload)
+			return r.json()
+
+		user_info = getUser()
+		
+		print "User name is", user_info['data']['username']
+
+		response['self_user'] = user_info['data']['username']
+
+		print "User has", user_info['data']['counts']['follows'], "followings"
+
+		response['followings'] = user_info['data']['counts']['follows']
+
+		def populateFollowings(user_id):
+			paginated = True
+			current_url = 'https://api.instagram.com/v1/users/'+str(user_id)+'/follows'
+			while paginated:
+				r = session.get(current_url, params=payload)
+				for item in r.json()['data']:
+					followees.append(item['id'])
+				if 'next_url' in r.json()['pagination']:
+					current_url = r.json()['pagination']['next_url']
+				else: 
+					paginated = False
+			print len(followees), "Followings found"
+			return True
+
+		print "Getting followings info..."
+		populateFollowings(user_info['data']['id'])
+
+		response['jobs_list'] = "ig_joblist:"+str(uuid.uuid4())
+		print "Creating jobs to get followings media...", response['jobs_list']
+
+		if os.environ['DEBUG_MODE'] == 'TRUE':
+			# Only get the first 5 followees to conserve time
+			print "[DEBUG] Getting only first 5 followees"
+			for user in followees[:5]:
+				job = q.enqueue(utils.getFolloweeData, user, payload['client_id'], payload['access_token'])
+				conn.sadd(response['jobs_list'], job.id)
+		else:
+			for user in followees:
+				job = q.enqueue(utils.getFolloweeData, user, payload['client_id'], payload['access_token'])
+				conn.sadd(response['jobs_list'], job.id)
+
+		return response
+	else:
+		return 'Missing access_token'
+
+@route('/refresh_state')
+def refresh_state():
+	if request.query.get('jobs_list') is not None:
+		response = {}
+
+		response['results'] = []
+
+		jobs_list = request.query.get('jobs_list')
+		print "Refreshing state", jobs_list
+
+		jobs = conn.smembers(jobs_list)
+		for job in jobs:
+			current_job = q.fetch_job(job)
+			if current_job.result is not None:
+				conn.srem(jobs_list, current_job.id)
+				response['results'].append(current_job.result)
+
+			elif current_job.status == 'failed':
+				conn.srem(jobs_list, current_job.id)
+
+		# If these are the last jobs:
+		if len(jobs) == 0:
+			response['done'] = True
+
+		return response
+
+	else:
+		return 'Missing jobs list'
 
 @route('/unfollow')
 def unfollow():
@@ -179,12 +146,4 @@ def follow():
 		r = session.post('https://api.instagram.com/v1/users/'+request.query.get('user_id')+'/relationship', params=payload)
 		return "Instagram replied:"+str(r.content)
 
-@route('/get_data')
-def get_data():
-	if request.query.get('access_token') is not None:
-		return main(request.query.get('access_token'))
-	else:
-		return 'Missing access_token'
-
 run(host='0.0.0.0', port=sys.argv[1])
-
---
+++ utils.py
@@ -0,0 +1,59 @@
+import requests
+from datetime import datetime, date, timedelta
+
+def getFolloweeData(user_id, client_id, access_token):
+
+	results = {}
+
+	# Call the IG servers
+	payload = { 
+		'client_id': client_id,
+		'access_token': access_token,
+	}
+	r = requests.get('https://api.instagram.com/v1/users/'+user_id+'/media/recent/', params=payload)
+
+	# Extract recent media from the response
+	recent_media = []
+
+	if r.status_code == 200: # If not, this user is probably visible to followers only
+		for item in r.json()['data']:
+			recent_media.append({'time':float(item['created_time']), 'user': item['user']['username'], 'likes': int(item['likes']['count'])})
+
+	results['last_photo_time'] = recent_media[0]['time']
+
+	# Calculate photos per day
+
+	days = {}
+	most_days_back = datetime.now().date()-datetime.fromtimestamp(recent_media[-1]['time']).date()
+	most_days_back = most_days_back.days
+
+	for day in xrange(0,most_days_back):
+		days[day] = 0
+
+	for photo in recent_media:
+		photo_day = datetime.now().date()-datetime.fromtimestamp(photo['time']).date()
+		photo_day = photo_day.days
+		if photo_day < most_days_back:
+			days[photo_day] += 1
+
+	num_of_photos = 0
+	for day in xrange(0,most_days_back):
+		num_of_photos += days[day]
+
+	results['photos_per_day'] = format(num_of_photos / float(most_days_back), '.2f')
+
+	# Calculate likes per photo
+
+	likes = 0
+	for photo in recent_media:
+		likes += photo['likes']
+
+	results['likes_per_photo'] = format(likes / float(len(recent_media)), '.2f')
+
+	# Additional metadata
+
+	results['username'] = recent_media[0]['user']
+
+	results['user_id'] = user_id
+
+	return results
